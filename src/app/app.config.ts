6febaefafdd50a9ca462132972799a53ac259ff0
pboucher44 <boucher.pierre44400@hotmail.fr>
2025-01-11 19:47:02 +0100

    Adding auth0 config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,5 +2,31 @@ import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core'
 import {provideRouter} from '@angular/router'
 
 import {routes} from './app-routing.module'
+import {authHttpInterceptorFn, provideAuth0} from '@auth0/auth0-angular'
+import {provideHttpClient, withInterceptors} from '@angular/common/http'
 
-export const appConfig: ApplicationConfig = {providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes)]}
+export const appConfig: ApplicationConfig = {providers: [
+  provideZoneChangeDetection({eventCoalescing: true}),
+  provideRouter(routes),
+  provideAuth0({
+    domain: 'mes-devoirs.eu.auth0.com',
+    clientId: '2Hj7GOD9k1ddIM60YDW09o5cZAXzTjVr',
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: 'https://mes-devoirs.eu.auth0.com/api/v2/',
+      scope: 'read:current_user openid profile',
+    },
+    httpInterceptor: {allowedList: [{
+      uri: 'http://localhost:8080/*',
+      tokenOptions: {authorizationParams: {
+        // The attached token should target this audience
+        audience: 'https://mes-devoirs.eu.auth0.com/api/v2/',
+
+        // The attached token should have these scopes
+        scope: 'read:current_user',
+      }},
+    }]},
+  }),
+  provideHttpClient(),
+  provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+]}
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core'
+import {Component, Inject} from '@angular/core'
+import {AuthService} from '@auth0/auth0-angular'
+import {AsyncPipe, CommonModule, DOCUMENT} from '@angular/common'
+import {concatMap, tap} from 'rxjs'
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
   standalone: true,
+  imports: [AsyncPipe, CommonModule],
 })
 export class AuthComponent {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService,
+    private http: HttpClient
+  ) {}
+  classrooms = {}
 
+  getMetadata() {
+    this.auth.user$
+      .pipe(
+        concatMap((user) =>
+          // Use HttpClient to make the call
+          this.http.get(
+            encodeURI('http://localhost:8080/v1/classrooms')
+          )
+        ),
+        tap((classrooms) => (this.classrooms = classrooms))
+      )
+      .subscribe()
+  }
 }
